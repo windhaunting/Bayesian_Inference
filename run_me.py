4d55606d4912736f01f6f460d834af93d61b6efa
fubao <freadream@gmail.com>
2017-11-10 15:03:01 -0500

    add_files

--- run_me.py
+++ run_me.py
@@ -3,12 +3,45 @@ import numpy as np
 import kaggle
 
 ################################################
-if __name__ == '__main__':
+    
+def testQuestionAProbDist(JLst, a):
+    '''
+    Implement a function to calculate P (J|α) with input arguments J, α and output P (J|α)
+    JLst: a sequence of jar
+    a is the model paameter deciding the next jar same as the previous one or not
+    '''
+ 
+    PJGivenA = 1          #prob of jar sequence given  \alpha
+    for i in range(1, len(JLst)):
+        PJGivenA = PJGivenA * a if JLst[i] == JLst[i-1] else PJGivenA * (1-a)
+                   
+    print ("PJGivenA: ", PJGivenA)
 
-	print('1a through 1l computation goes here ...')
 
-	################################################
+    
+if __name__== "__main__":
 
+    ################################################
+    print('1a through 1l computation goes here ...')
+    JLst = [0, 1, 1, 0, 1]
+    a = 0.75
+    
+    testQuestionAProbDist(JLst, a)
+    
+    JLst = [0, 0, 1, 0, 1]
+    a = 0.2
+    testQuestionAProbDist(JLst, a)
+    
+    
+    JLst = [1,1,0,1,0,1]
+    a = 0.2
+    testQuestionAProbDist(JLst, a)
+    
+    JLst = [0,1,0,1,0,0]
+    a = 0.2
+    testQuestionAProbDist(JLst, a)
+    
+'''
 	print('1m')
 	lengths = [10, 15, 20, 25]
 	prediction_prob = list()
@@ -25,3 +58,5 @@ if __name__ == '__main__':
 	print('Writing output to ', file_name)
 	kaggle.kaggleize(np.array(prediction_prob), file_name)
 
+
+'''
